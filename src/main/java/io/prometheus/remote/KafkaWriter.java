bb758d438560ba3990622671496e6bb7797fdec8
Ramon Gordillo <rgordill@redhat.com>
2019-06-30 18:14:38 +0200

    So easy that makes me ashamed

--- src/main/java/io/prometheus/remote/KafkaWriter.java
+++ src/main/java/io/prometheus/remote/KafkaWriter.java
@@ -1,16 +1,37 @@
 package io.prometheus.remote;
 
-import javax.ws.rs.GET;
+import java.io.IOException;
+
+import javax.inject.Inject;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.xerial.snappy.Snappy;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.google.protobuf.util.JsonFormat;
+
+import io.smallrye.reactive.messaging.annotations.Emitter;
+import io.smallrye.reactive.messaging.annotations.Stream;
+import prometheus.Remote.WriteRequest;
 
 @Path("/write")
 public class KafkaWriter {
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "hello";
+	static Logger logger = LoggerFactory.getLogger(KafkaWriter.class);
+	
+	@Inject
+	@Stream("prometheus") // Emit on the channel 'metrics'
+	Emitter<String> metrics;
+	
+    @POST
+    public void write( byte[] byteArray) throws InvalidProtocolBufferException, IOException {
+    	
+		WriteRequest wr = WriteRequest.parseFrom(Snappy.uncompress(byteArray));
+
+		metrics.send(JsonFormat.printer().print(wr));
     }
 }
